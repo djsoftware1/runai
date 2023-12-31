013b2bd52f1c2df63d9bed8a692a59bd8896d7ad
davidjoffe <david@djoffe.com>
2024-01-01 00:03:57 +0200

    Copyright refactoring task update

--- tasks/copyright/settings.py
+++ tasks/copyright/settings.py
@@ -6,12 +6,12 @@ do_refactor = True
 refactor_wildcard = "*.cpp"
 #refactor_wildcard = "*.h"
 refactor_codetype = "cpp"
-refactor_wildcards = ["*.cpp", "*.h", "*.php"]
+refactor_wildcards = ["*.cpp", "*.h", "*.php", "*.py"]
 #refactor_wildcards=["*.php"]
 
 # Don't change the actual function itself
 #refactor_matches = "^[^\n]*Copyright [^\n]*\n"
 refactor_matches = "Copyright "
 #refactor_multiline_matches = []
-# If already includes 2023 don't even send it
-refactor_negmatches =["2023"]
+# If already includes latest year don't even send it
+refactor_negmatches =["2024"]
