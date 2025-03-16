056f4b7772cc5953bcc309d986c95a7c87f84afa
David Joffe <david@djoffe.com>
2025-03-16 04:54:47 +0200

    Version

--- djversion.py
+++ djversion.py
@@ -1,9 +1,10 @@
-# Copyright (C) David Joffe 2023-2024
+# djversion.py: Small "Version" helper class to manage and return current version of the application
+# Copyright (C) David Joffe 2023-2025
 # Version helper
 
 class Version:
     def __init__(self):
-        self.current_version = '0.8.2'
+        self.current_version = '0.8.3'
 
     def get_version(self):
         return self.current_version
