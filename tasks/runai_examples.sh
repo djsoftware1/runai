b158e6aa6ffee48759ae0b1f1315102072f44ab5
David Joffe <david@djoffe.com>
2025-12-31 04:54:10 +0200

    Add Lennard-Jones molecular sim python script example

---
+++ tasks/runai_examples.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+runai -t "Write a Python script that implements a Lennard-Jones molecular simulation of 1000 molecules in a loop with simulation deltatime 0.002 seconds" create -o ljones.py
+
+#runai --openai -t "Write a Python script that implements a Lennard-Jones molecular simulation of 1000 molecules in a loop with simulation deltatime 0.002 seconds" create -o ljones.py
